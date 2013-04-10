b220243b07335096d85718a612fd3a48b0bf24da
Stoica Vlad <vlad.stoick@yahoo.co.uk>
2013-04-10 11:34:49 +0300

    first row view type works showing roTitle and enTitle

---
+++ src/com/vladstoick/gotocinema/AparitiiCinemaAdapter.java
@@ -0,0 +1,48 @@
+package com.vladstoick.gotocinema;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.view.*;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import android.app.Activity;
+public class AparitiiCinemaAdapter extends ArrayAdapter<AparitiiCinema>{
+	Context context; 
+    int layoutResourceId;    
+    ArrayList<AparitiiCinema> data = new ArrayList<AparitiiCinema>();
+	public AparitiiCinemaAdapter(Context context, int textViewResourceId, ArrayList<AparitiiCinema> data) {
+		super(context, textViewResourceId, data);
+		this.layoutResourceId = textViewResourceId;
+        this.context = context;
+        this.data = data;
+	}
+	@Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View row = convertView;     
+        AparitiiCinemaRowHandler holder = null;
+        if(row == null)
+        {
+            LayoutInflater inflater = ((Activity)context).getLayoutInflater();
+            row = inflater.inflate(layoutResourceId, parent, false);
+            
+            holder = new AparitiiCinemaRowHandler();
+            holder.roTitle = (TextView)row.findViewById(R.id.rowRoTitle);
+            holder.enTitle = (TextView)row.findViewById(R.id.rowEnTitle);
+            row.setTag(holder);
+        }
+        else
+        {
+            holder = (AparitiiCinemaRowHandler)row.getTag(); 
+        }
+        AparitiiCinema aparitie = data.get(position);
+        holder.enTitle.setText(aparitie.enTitle);
+        holder.roTitle.setText(aparitie.roTitle);
+        
+        return row;
+    }
+	static class AparitiiCinemaRowHandler
+    {
+        TextView roTitle, enTitle;
+    }
+}
