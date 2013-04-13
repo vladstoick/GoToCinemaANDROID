43c759711966fa02878073661b0be86b91ab694a
Stoica Vlad <vlad.stoick@yahoo.co.uk>
2013-04-13 12:18:39 +0300

    removed mapquest api went back to google maps

--- src/com/vladstoick/gotocinema/FilmView.java
+++ src/com/vladstoick/gotocinema/FilmView.java
@@ -1,79 +1,35 @@
 package com.vladstoick.gotocinema;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.webkit.WebView;
+import android.widget.TextView;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-import com.mapquest.android.maps.GeoPoint;
-import com.mapquest.android.maps.MapActivity;
-import com.mapquest.android.maps.MapView;
-import com.mapquest.android.maps.MyLocationOverlay;
-import com.mapquest.android.maps.RouteManager;
-public class FilmView extends MapActivity {
-    protected MapView map;
-    private MyLocationOverlay myLocationOverlay;
+public class FilmView extends Activity {
+
     AparitiiCinema currentInfo;
+    LatLng pozitieCinema = new LatLng(44.419560, 26.1266510);
+    private GoogleMap mMap;
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
-      setContentView(R.layout.activity_film_view);
-      Intent intent = getIntent();
-      currentInfo = intent.getParcelableExtra("INFO");
-      setTitle(currentInfo.enTitle);
-      setupMapView();
-      setupMyLocation();
-      WebView itinerary = (WebView) findViewById(R.id.itinerary);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_film_view);
+        Intent intent = getIntent();
+        currentInfo = intent.getParcelableExtra("INFO");
+        setTitle(currentInfo.enTitle);
+        //TODO currentInfo.lat && currentInfo.lng de implementat
 
-      // pass in your AppKey to RouteManager when not extending MapActivity 
-      RouteManager routeManager = new RouteManager(this);
-
-      // generate and display the route narrative (itinerary) 
-      routeManager.setItineraryView(itinerary);
-      routeManager.createRoute("San Francisco, CA", "Fremont, CA");
-    }
-
-    // set your map and enable default zoom controls 
-    private void setupMapView() {
-      this.map = (MapView) findViewById(R.id.mapQuestView);
-      map.setBuiltInZoomControls(true);
-    }
-
-    // set up a MyLocationOverlay and execute the runnable once we have a location fix 
-    private void setupMyLocation() {
-      this.myLocationOverlay = new MyLocationOverlay(this, map);
-      myLocationOverlay.enableMyLocation();
-      myLocationOverlay.runOnFirstFix(new Runnable() {
-        @Override
-        public void run() {
-//          GeoPoint currentLocation = myLocationOverlay.getMyLocation();
-          map.getController().setZoom(16);
-          //TODO de luat o locatie pe bune
-
-          map.getOverlays().add(myLocationOverlay);
-          map.getController().setCenter(new GeoPoint(44.419560,26.1266510));
-//          myLocationOverlay.setFollowing(true);
-        }
-      });
-    }
-
-    // enable features of the overlay 
-    @Override
-    protected void onResume() {
-      myLocationOverlay.enableMyLocation();
-      myLocationOverlay.enableCompass();
-      super.onResume();
-    }
-
-    // disable features of the overlay when in the background 
-    @Override
-    protected void onPause() {
-      super.onPause();
-      myLocationOverlay.disableCompass();
-      myLocationOverlay.disableMyLocation();
-    }
-
-    @Override
-    public boolean isRouteDisplayed() {
-      return false;
+        mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.mapView)).getMap();
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(pozitieCinema));
+        mMap.animateCamera(CameraUpdateFactory.zoomBy(15));
+        mMap.setMyLocationEnabled(true);
+        mMap.addMarker(new MarkerOptions()
+                .position(pozitieCinema)
+                .title("Cinema-ul cautat de tine"));
     }
 }
