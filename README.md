ce2670a3f84da6001e80a7cd0ac76b556dc26b94
Stoica Vlad <vlad.stoick@yahoo.co.uk>
2013-05-02 17:36:09 +0300

    first slidingmenu

--- README.md
+++
@@ -1,4 +0,0 @@
-GoToCinemaANDROID
-=================
-
-GoToCinema este o aplicatie dezvoltata de Alexandru Clapa (iOS), Vlad Stoica (Android). Ofera o lista cu filmele la cere userul poate ajunge pe baza locatiei si orei curente.
--- src/com/vladstoick/gotocinema/MainActivity.java
+++ src/com/vladstoick/gotocinema/MainActivity.java
@@ -1,149 +1,68 @@
 package com.vladstoick.gotocinema;
 
-import android.app.ProgressDialog;
-import android.content.Context;
-import android.content.Intent;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.Fragment;
 import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import com.slidingmenu.lib.app.SlidingFragmentActivity;
-public class MainActivity extends SlidingFragmentActivity implements LocationListener {
-    static private TextView timeUsed,locationUsedText;
-    static ProgressDialog pd=null;
-    static private int hourUsed, minuteUsed;
-    static boolean  isUsingLocation = true,setALocation = false;
-    private LocationManager locationManager;
-    private String provider;
-    static Location locationUsed;
-    private static ArrayList<AparitiiCinema>filme = new ArrayList<AparitiiCinema>(); 
-    public static ArrayList<AparitiiCinema> getFilme() {
-		return filme;
+
+import com.actionbarsherlock.app.SherlockDialogFragment;
+import com.actionbarsherlock.app.SherlockFragment;
+import com.slidingmenu.lib.SlidingMenu;
+
+public class MainActivity extends BaseActivity implements OnFragmentInteractionListener {
+	static String TAGCALCULATE ="CalculateMainMenuFragment";
+	private Fragment mContent;
+	
+	public MainActivity(){
+		super(R.string.app_name);
+		
 	}
-	public static void setFilme(ArrayList<AparitiiCinema> filmeToBeFilled) {
-		filme = filmeToBeFilled;
-	}
-	public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        setBehindContentView(R.layout.activity_cinema_list);
-        getSlidingMenu().setBehindOffset(200);
-        Calendar c = Calendar.getInstance();
-        timeUsed = (TextView) findViewById(R.id.hourUsed);
-        locationUsedText = (TextView) findViewById(R.id.locationUsed);
-        hourUsed = c.get(Calendar.HOUR_OF_DAY);
-        minuteUsed = c.get(Calendar.MINUTE);
-        updateHour();
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        Criteria criteria = new Criteria();
-        provider = locationManager.getBestProvider(criteria, false);
-//        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-        Location location = locationManager.getLastKnownLocation(provider);
-        if (location != null) {
-            System.out.println("Provider " + provider + " has been selected.");
-            onLocationChanged(location);
-          } else {
-            Toast.makeText(this, "Trebuie să activaţi o metodă de localizare", Toast.LENGTH_LONG);
-          }
-        pd = ProgressDialog.show(this, "Preluare filme", "Vă rugăm aşteptaţi", true);
-		RequestTask newV = new RequestTask(1,MainActivity.this);
-		newV.execute("http://parsercinema.eu01.aws.af.cm/date.json");
-//		SlidingMenu menu = new SlidingMenu(this);
-//	    menu.setMode(SlidingMenu.LEFT);
-//	    menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
-//	    menu.setFadeDegree(0.35f);
-//	    menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
-//	    menu.setMenu(R.layout.activity_film_view);
-    }
-    //HOUR SETUP
-    static Handler mHandler = new Handler() {
-        @Override
-        public void handleMessage(Message m) {
-            Bundle b = m.getData();
-            hourUsed = b.getInt("set_hour");
-            minuteUsed = b.getInt("set_minute");
-            updateHour();
-        }
-    };
-    private static void updateHour() {
-        String deAfisat = Utils.getStringFromDate(Utils.getDateFromHourAndMinuteInts(hourUsed, minuteUsed));
-        timeUsed.setText("Ora folosita " + deAfisat);
-    }
-    public void changeTime(View v) {
-        Bundle b = new Bundle();
-        b.putInt("set_hour", hourUsed);
-        b.putInt("set_minute", minuteUsed);
-        ChangeDialog timePicker = new ChangeDialog(mHandler);
-        timePicker.setArguments(b);
-        android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
-        android.support.v4.app.FragmentTransaction ft = fm.beginTransaction();
-        ft.add(timePicker, "time_picker");
-        ft.commit();
-    }
-    public static Date getDate()
-    {
-    	return Utils.getDateFromHourAndMinuteInts(hourUsed, minuteUsed);
-    }
-    //BUTOTONS AND FUNCTIONALITY
-    public void calculateCinema(View view) {
-    	if(setALocation==false)
-    	{
-    		Toast.makeText(this, "Aplicaţia încă te localizează", Toast.LENGTH_SHORT).show();
-    	}
-    	else{
-    		pd = ProgressDialog.show(this, "Calcuare distanţă", "Vă rugăm aşteptaţi", true);
-    		RequestTask newV = new RequestTask(2,MainActivity.this);
-    		newV.execute("http://parsercinema.eu01.aws.af.cm/getDistance.php?lat="+locationUsed.getLatitude()+"&lng="+locationUsed.getLongitude());
-    	}
-       
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.settings, menu);
-        return true;
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_enterSettings:
-                Intent intent = new Intent(this, SettingsActivity.class);
-                startActivity(intent);
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-    //LOCATION
+	
 	@Override
-	public void onLocationChanged(Location location) {
-		System.out.println("location is"+location.getLongitude());
-		if(MainActivity.isUsingLocation==true)
-		{
-			locationUsed=location;
-			MainActivity.setALocation=true;
-			System.out.println(setALocation);
-			locationUsedText.setText("Poziţia folosită: locaţia ta");
-		}
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		
+		if (savedInstanceState != null)
+			mContent = getSupportFragmentManager().getFragment(savedInstanceState, "mContent");
+		if (mContent == null)
+			mContent = new CalculateMainMenuFragment();
+		
+		setContentView(R.layout.content_frame);
+		getSupportFragmentManager()
+		.beginTransaction()
+		.replace(R.id.content_frame, mContent , TAGCALCULATE).commit();
+		
+		setBehindContentView(R.layout.menu_frame);
+		getSupportFragmentManager()
+		.beginTransaction()
+		.replace(R.id.menu_frame, new SlidingMenuFragment()).commit();
+		
+		getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+		setSlidingActionBarEnabled(true);
 	}
-	public static Location getLocation()
+	
+	@Override
+	public void onSaveInstanceState(Bundle outState){
+		super.onSaveInstanceState(outState);
+		getSupportFragmentManager().putFragment(outState, "mContent", mContent);
+	}
+	
+	public void switchContent(Fragment fragment){
+		mContent = fragment;
+		getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, fragment).commit();
+		getSlidingMenu().showContent();
+	}
+	public void showTimePicker(View view)
 	{
-		return locationUsed;
+	    SherlockDialogFragment newFragment = new TimePickerFragment();
+	    newFragment.show(getSupportFragmentManager(), "timePicker");
 	}
 	@Override
-	public void onProviderDisabled(String provider) {}
-	@Override
-	public void onProviderEnabled(String provider) {}
-	@Override
-	public void onStatusChanged(String provider, int status, Bundle extras) {}
+	public void onSettedATime(int hour, int minute) {
+		if( getSupportFragmentManager().findFragmentByTag(TAGCALCULATE)!=null)
+			((TimePickerFragment) getSupportFragmentManager().findFragmentByTag(TAGCALCULATE)).updateTime(hour,minute);
+		
+	}
+	
+	
+
 }
