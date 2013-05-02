012de4f0f1876faa2c07c6592fcbeeb3e33cb978
Stoica Vlad <vlad.stoick@yahoo.co.uk>
2013-05-03 00:22:38 +0300

    added packages

--- src/com/vladstoick/gotocinema/LocationFragmentListener.java
+++
@@ -1,7 +0,0 @@
-package com.vladstoick.gotocinema;
-
-import android.location.Location;
-
-public interface LocationFragmentListener {
-	public void updatedCurrentLocation(Location location);
-}
--- src/com/vladstoick/gotocinema/OnFragmentInteractionListener.java
+++
@@ -1,8 +0,0 @@
-package com.vladstoick.gotocinema;
-
-import android.location.Location;
-
-public interface OnFragmentInteractionListener {
-	public void onSettedATime(int hour,int minute);
-	public Location getCurrentLocation();
-}
