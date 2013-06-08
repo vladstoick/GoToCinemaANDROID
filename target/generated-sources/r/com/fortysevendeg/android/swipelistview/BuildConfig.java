06b130b4ea179e64933e86d1ecc198e46e166688
Stoica Vlad <vlad.stoick@yahoo.co.uk>
2013-06-08 16:47:12 +0300

    removed some files

--- target/generated-sources/r/com/fortysevendeg/android/swipelistview/BuildConfig.java
+++
@@ -1,5 +0,0 @@
-package com.fortysevendeg.android.swipelistview;
-
-public final class BuildConfig {
-  public static final boolean DEBUG = true;
-}
--- target/generated-sources/r/com/vladstoick/gotocinema/BuildConfig.java
+++
@@ -1,5 +0,0 @@
-package com.vladstoick.gotocinema;
-
-public final class BuildConfig {
-  public static final boolean DEBUG = true;
-}
